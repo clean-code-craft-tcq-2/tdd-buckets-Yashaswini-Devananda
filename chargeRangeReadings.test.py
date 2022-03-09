235a5da9749f52880610b4c38741d51fab82f46b
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-03-09 22:54:43 +0530

    Update and rename typewise_alert.test.py to chargeRangeReadings.test.py

---
+++ chargeRangeReadings.test.py
@@ -0,0 +1,14 @@
+import unittest
+import chargeRangeReadings
+
+class chargeRangeReadingsTest(unittest.TestCase):
+  
+  def test(self):
+    getFreqOfChargeRanges([4,2])
+
+
+if __name__ == '__main__':
+  unittest.main()
+  
+  
+  
