075232a24649d97c4cb2f968232592f98a808cb0
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-03-18 10:40:15 +0530

    Update chargeRangeReadings.test.py

--- chargeRangeReadings.test.py
+++ chargeRangeReadings.test.py
@@ -6,8 +6,10 @@ class chargeRangeReadingsTest(unittest.TestCase):
   def test(self):
 #     response = chargeRangeReadings.getFreqOfChargeRanges([6,4,5])
 #     print(response)
-    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([6,4,5]) == " 4-6, 3 \n")
 #     self.assertTrue(response == " 4-6, 3 \n")
+
+    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([6,4,5]) == " 4-6, 3 \n")
+    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([1,1,1,1,1,1,1) == " 1-1, 7 \n")
 
 
 if __name__ == '__main__':
