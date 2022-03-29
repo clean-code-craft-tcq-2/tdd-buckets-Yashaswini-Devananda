e7cbab6e63c033a1f5794c0b22903bca247a2dbd
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-03-29 16:22:59 +0530

    Update chargeRangeReadings.test.py

--- chargeRangeReadings.test.py
+++ chargeRangeReadings.test.py
@@ -8,7 +8,7 @@ class chargeRangeReadingsTest(unittest.TestCase):
     self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([0], chargeRangeReadings.ADC_12Bit) == "0-0, 1\n")
     self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([4094], chargeRangeReadings.ADC_12Bit) == "10-10, 1\n")
     self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([4094,4095], chargeRangeReadings.ADC_12Bit) == "10-10, 1\n")#ignore error readings
-    print (chargeRangeReadings.getFreqOfChargeRanges([0,4000], chargeRangeReadings.ADC_12Bit))
+#     print (chargeRangeReadings.getFreqOfChargeRanges([0,4000], chargeRangeReadings.ADC_12Bit))
 #     self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([0,4000], chargeRangeReadings.ADC_12Bit) == "0-0, 1\n10-10, 1 \n")
     
     
