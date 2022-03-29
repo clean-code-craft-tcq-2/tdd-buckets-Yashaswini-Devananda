0ea97d185d1b455dfb7a52d570538375f966dafa
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-03-29 10:58:58 +0530

    Update chargeRangeReadings.py

--- chargeRangeReadings.py
+++ chargeRangeReadings.py
@@ -1,14 +1,28 @@
+ADC_12Bit = {"max_permissible_value" : 4094 , "intercept" : 0 , "max_current_range" : 10}
+ADC_10Bit = {"max_permissible_value" : 511 , "intercept" : 511 , "max_current_range" : 15}
+
+
+def map_adc_values_to_range(A2Dvalues, ADC_dict):
+    result=[]
+    for value in A2Dvalues:
+        value -= ADC_dict["intercept"]
+        if value<=ADC_dict["max_permissible_value"] :
+            result.append(round(((value)/ADC_dict["max_permissible_value"])*ADC_dict["max_current_range"]))
+    return result
+
+
 def getSortedListofUniqueItems(listOfItems):
     setofList = set(listOfItems)
     sortedList = list(setofList)
     sortedList.sort()
-    
     return sortedList
+    
     
 def get_ListOfContinousRanges(charge_readings):
     listOfItems = getSortedListofUniqueItems(charge_readings)
-    # print(listOfItems)
     listOfRange=[]
+    if len(listOfItems) == 0:
+        return []
     rangeDict = { "min":listOfItems[0],"max":listOfItems[0],"freq":0}
     for item in listOfItems:
         if(item <= (rangeDict["max"]+1)):
@@ -17,9 +31,7 @@ def get_ListOfContinousRanges(charge_readings):
             listOfRange.append(rangeDict.copy())
             rangeDict["min"] = item
             rangeDict["max"] = item
-    
     listOfRange.append(rangeDict.copy()) # store the last items
-    
     return listOfRange
     
     
@@ -28,7 +40,6 @@ def updateFrequencyOfReading(reading,listOfRange):
         if(reading <= rangeItem["max"]):
             rangeItem["freq"]=rangeItem["freq"]+1
             return listOfRange
-    
     return listOfRange
     
     
@@ -37,15 +48,14 @@ def updateFreqOfRange(listOfRange,charge_readings):
         listOfRange = updateFrequencyOfReading(item,listOfRange)
     return listOfRange        
     
+    
 def output_to_console(listOfRange):
-    responseText = ""
     for item in listOfRange:
-        responseText = f"{responseText} {item['min']}-{item['max']}, {item['freq']} \n"
-        # print (responseText)
-    return responseText
-            
-def getFreqOfChargeRanges(InputList):
-    charge_readings = InputList
+        print (f"{item['min']}-{item['max']}, {item['freq']}")
+    
+
+def getFreqOfChargeRanges(InputList, ADC_dict):
+    charge_readings = map_adc_values_to_range(InputList,ADC_dict)
     listOfRange = get_ListOfContinousRanges(charge_readings)
     listOfRange = updateFreqOfRange(listOfRange,charge_readings)
     responseText = output_to_console(listOfRange)
