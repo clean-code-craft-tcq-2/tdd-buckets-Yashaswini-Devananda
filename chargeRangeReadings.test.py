2a9ffad4e3f6960b857c29efdfd9b6975b00a60f
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-03-29 11:21:59 +0530

    Update chargeRangeReadings.test.py

--- chargeRangeReadings.test.py
+++ chargeRangeReadings.test.py
@@ -4,8 +4,8 @@ import chargeRangeReadings
 class chargeRangeReadingsTest(unittest.TestCase):
   
   def test(self):
-    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([4094], chargeRangeReadings.ADC_12Bit) == " 10-10, 1 \n")
     print (chargeRangeReadings.getFreqOfChargeRanges([4094], chargeRangeReadings.ADC_12Bit))
+    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([4094], chargeRangeReadings.ADC_12Bit) == " 10-10, 1 \n")
     
 if __name__ == '__main__':
   unittest.main()
