207b4594a2a3b20f3c7190d7d7287d74ce451d4f
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-03-29 15:26:56 +0530

    Update chargeRangeReadings.test.py

--- chargeRangeReadings.test.py
+++ chargeRangeReadings.test.py
@@ -8,7 +8,7 @@ class chargeRangeReadingsTest(unittest.TestCase):
     self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([0], chargeRangeReadings.ADC_12Bit) == "0-0, 1\n")
     self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([4094], chargeRangeReadings.ADC_12Bit) == "10-10, 1\n")
     self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([4094,4095], chargeRangeReadings.ADC_12Bit) == "10-10, 1\n")#ignore error readings
-    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([0,100,200,400,500,1000,1500,2500,3000,3500,4000], chargeRangeReadings.ADC_12Bit) == "0-2, 6 \n 4-4, 1 \n 6-7, 2 \n 9-10, 2 \n")
+    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([0,100,200,400,500,1000,1500,2500,3000,3500,4000], chargeRangeReadings.ADC_12Bit) == "0-2, 6\n 4-4, 1\n 6-7, 2\n 9-10, 2\n")
     
     
     self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([], chargeRangeReadings.ADC_10Bit) == "")
@@ -16,7 +16,7 @@ class chargeRangeReadingsTest(unittest.TestCase):
     self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([511], chargeRangeReadings.ADC_10Bit) == "0-0, 1\n")
     self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([1022], chargeRangeReadings.ADC_10Bit) == "15-15, 1\n")
     self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([1022,1023], chargeRangeReadings.ADC_10Bit) == "15-15, 1\n")#ignore error readings
-    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([0,100,150,300,500,70,1500,700,600,2000,1022], chargeRangeReadings.ADC_10Bit) == "-15--15, 1 \n -13--11, 3 \n -6--6, 1 \n 0-0, 1 \n 3-3, 1 \n 6-6, 1 \n 15-15, 1 \n")
+#     self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([0,100,150,300,500,70,1500,700,600,2000,1022], chargeRangeReadings.ADC_10Bit) == "-15--15, 1 \n -13--11, 3 \n -6--6, 1 \n 0-0, 1 \n 3-3, 1 \n 6-6, 1 \n 15-15, 1 \n")
 
     
 if __name__ == '__main__':
