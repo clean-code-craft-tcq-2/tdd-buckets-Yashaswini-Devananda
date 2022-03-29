55622dbf9f61cac47a2c0bd61c1fd5fa28e68a4a
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-03-29 15:10:53 +0530

    Update chargeRangeReadings.test.py

--- chargeRangeReadings.test.py
+++ chargeRangeReadings.test.py
@@ -5,12 +5,17 @@ class chargeRangeReadingsTest(unittest.TestCase):
   
   def test(self):
     self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([], chargeRangeReadings.ADC_12Bit) == "")
-    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([], chargeRangeReadings.ADC_10Bit) == "")
-    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([4094], chargeRangeReadings.ADC_12Bit) == "10-10, 1\n")
     self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([0], chargeRangeReadings.ADC_12Bit) == "0-0, 1\n")
-    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([1022], chargeRangeReadings.ADC_10Bit) == "15-15, 1\n")
-    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([511], chargeRangeReadings.ADC_10Bit) == "0-0, 1\n")
+    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([4094], chargeRangeReadings.ADC_12Bit) == "10-10, 1\n")
+    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([4094,4095], chargeRangeReadings.ADC_12Bit) == "10-10, 1\n")#ignore error readings
+    
+    
+    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([], chargeRangeReadings.ADC_10Bit) == "")
     self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([0], chargeRangeReadings.ADC_10Bit) == "-15--15, 1\n")
+    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([511], chargeRangeReadings.ADC_10Bit) == "0-0, 1\n")
+    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([1022], chargeRangeReadings.ADC_10Bit) == "15-15, 1\n")
+    self.assertTrue(chargeRangeReadings.getFreqOfChargeRanges([1022,1023], chargeRangeReadings.ADC_10Bit) == "15-15, 1\n")#ignore error readings
+
     
 if __name__ == '__main__':
   unittest.main()
